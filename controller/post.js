84a264138a1067d7c5d7900d564ca298425a9e78
San <sanshwe330@gmail.com>
2022-07-06 20:05:27 +0630

    edit missing response in createPost component

--- controller/post.js
+++ controller/post.js
@@ -4,7 +4,6 @@ const FeaturedPost = require("../models/featuredPost");
 const cloudinary = require("../cloud");
 const { isValidObjectId } = require("mongoose");
 const post = require("../models/post");
-const { remove } = require("../models/post");
 
 const FEATURED_POST_COUNT = 4;
 
@@ -44,7 +43,7 @@ exports.createPost = async (req, res) => {
     return res.status(401).json({ error: "Please use unique slug" });
 
   const newPost = new Post({ title, meta, content, slug, author, tags });
-
+  console.log(newPost);
   if (file) {
     const { secure_url: url, public_id } = await cloudinary.uploader.upload(
       file.path
@@ -62,6 +61,8 @@ exports.createPost = async (req, res) => {
       id: newPost._id,
       title,
       content,
+      slug,
+      meta,
       thumbnail: newPost.thumbnail?.url,
       author: newPost.author,
     },
